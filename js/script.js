f53c297ff2984578eb91dab45e5348dd5c7fec09
Arkingel Vassell <arkingelvassell@gmail.com>
2018-11-08 23:27:24 +0000

    rockpaperscissors

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@
 
 // DOCUMENT READY FUNCTION BELOW
 
+$("shoot").click(function() {
+    $("#userchoice").text($("input").val());
+});
+$("shoot").click(function() {
+    $("#computerChoice").text($("input").val());
+});
+$("shoot").click(function() {
+    $("#winner").text($("input").val());
+});
