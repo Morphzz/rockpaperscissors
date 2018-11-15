50742c3992213cec5362fe8552efb2f17337161c
Arkingel Vassell <arkingelvassell@gmail.com>
2018-11-15 23:36:29 +0000

    yay

--- js/script.js
+++ js/script.js
@@ -4,17 +4,27 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-
+var userChoice = "";
+var computerChoice = "";
+var winner = "";
+var randomNumber = 0;
 
 
 // DOCUMENT READY FUNCTION BELOW
 
-$("shoot").click(function() {
-    $("#userchoice").text($("input").val());
+
+    
+$("#shoot").click(function() {
+    userChoice = $("input").val();
+    $("#userChoice").text(userChoice);
+    
+   randomNumber = Math.random()
+   if(randomNumber < .333) {
+        $("#computerChoice").text("nice try"); 
+   }
+   
+  
+    console.log()
 });
-$("shoot").click(function() {
-    $("#computerChoice").text($("input").val());
-});
-$("shoot").click(function() {
-    $("#winner").text($("input").val());
-});
+
+
